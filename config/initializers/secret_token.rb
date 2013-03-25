3b21dd7655ac38ad73981d8f2d35a9717d751719
Mark Maximus Muskardin <mark.muskardin@gmail.com>
2013-03-25 10:52:08 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnicornFarm::Application.config.secret_token = '805138847926abcde897934937c847bc950bd91be6d4eeeb65f5988da54287a6db44ff9c895d8734205c372b55269b013795300f4f2eb209e74b9aa303dde014'
