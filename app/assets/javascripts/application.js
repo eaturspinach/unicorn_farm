103860a8452b7e578663b5c34f180a9c5b913731
Mark Maximus Muskardin <mark.muskardin@gmail.com>
2013-03-25 12:59:58 -0700

    added topbar.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require foundation
+//= require foundation.topbar
+//= require main
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,7 @@
+$(document).foundation('topbar', {
+	index : 0,
+	stickyClass : 'sticky',
+	custom_back_text: true, // Set this to false and it will pull the top level link name as the back text
+	back_text: 'Back', // Define what you want your custom back text to be if custom_back_text: true
+	init : false
+});
