d0eb9cebf74c6a00b16a8b597fde9332b3813f37
Mark Maximus Muskardin <mark.muskardin@gmail.com>
2013-03-26 14:28:07 -0700

    Created authentication model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130326211434) do
+ActiveRecord::Schema.define(:version => 20130326212733) do
 
   create_table "appointments", :force => true do |t|
     t.datetime "date"
     t.time     "time"
     t.integer  "user_id"
     t.text     "notes"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "authentications", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
