dc101ea5d48e233ae1e9046c35d460d2eef849f3
Mark Maximus Muskardin <mark.muskardin@gmail.com>
2013-03-27 00:41:36 -0700

    added appointment new form error messages, added flash as well

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,5 +4,14 @@ class AppointmentsController < ApplicationController
   end
 
   def create
+    @appointment = Appointment.create(params[:appointment])
+    if @appointment.save
+      respond_to do |format|
+        redirect_to user_path
+        flash.now[:success] = "Appointment created!"
+      end
+    else
+      render :new, :flash => {:error => "Very bad"}
+    end
   end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,5 @@
 class Appointment < ActiveRecord::Base
   attr_accessible :date, :notes, :time, :user_id
+
+  validates_presence_of :date, :time
 end
